8f7d39c2f8e0357b3a7606bf344fab94d8614923
Barbora Skálová <baryska@gmail.com>
2022-03-23 17:46:42 +0100

    create generator of random sentences

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 const zelenina = [
-    'rajče',
-    'okurka',
-    'paprika',
-    'mrkev',
-    'brambory',
-    'celer',
-    'zelí',
-    'květák',
-    'ředkvičky',
-    'brokolice'
-]
+  "rajče",
+  "okurka",
+  "paprika",
+  "mrkev",
+  "brambory",
+  "celer",
+  "zelí",
+  "květák",
+  "ředkvičky",
+  "brokolice",
+];
 
-const ovoce = [
-    'jablko',
-    'hruška',
-    'jahody',
-    'třešně'
-]
+const ovoce = ["jablko", "hruška", "jahody", "třešně"];
+
+const zdravejidlo = zelenina.concat(ovoce);
+
+const cisla = [1, 5, 300, 3, 4];
+
+// console.log(Math.floor(Math.random() * 6) + 1);
+
+const kostka = document.querySelector("h3");
+const tlacitko = document.querySelector("button");
+tlacitko.onclick = () => {
+  const nahodneCislo = Math.floor(Math.random() * 6);
+  kostka.textContent = vety[nahodneCislo];
+};
 
 const vety = [
-   "Zůstaň stát",
-   "Přeskoč o dvě pole dopředu",
-   "Dej se doprava",
-   "Dej se doleva",
-   "Jdi rovně",
-   "Jedno kolo stojíš" 
-]
+  "Zůstaň stát",
+  "Přeskoč o dvě pole dopředu",
+  "Dej se doprava",
+  "Dej se doleva",
+  "Jdi rovně",
+  "Jedno kolo stojíš",
+];
 
+console.log(vety[5]);
 
 // index
 // length
@@ -37,6 +46,3 @@ const vety = [
 // includes
 // sort
 // Math.random()
-
-
-
